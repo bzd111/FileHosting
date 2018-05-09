89feeb5779515efdc6d8098c6638f05f9a668e15
zhangcheng <1240450484zxc@gmail.com>
2018-05-09 20:15:51 +0800

    [Add] models functions

--- models.py
+++ models.py
@@ -4,7 +4,25 @@ import os
 import uuid
 import magic
 from datetime import datetime
+
+
+try:
+    from urllib import quote_plus # python2
+except ImportError:
+    from urllib.parse import quote_plus # python3
+
+
+import cropresize2
+import short_url
+from PIL import Image
+from flask import abort, request
+from werkzeug.utils import cached_property
+
+
+from libs.mimes import UDIO_MIMES, IMAGE_MIMES, VIDEO_MIMES
+from libs.utils import get_file_md5, get_file_path
 from ext import db
+
 
 class PasteFile(db.Model):
 
@@ -36,6 +54,42 @@ class PasteFile(db.Model):
         self._size = size
         self._filehash = filehash
         self._filemd5 = filemd5
+
+    @staticmethod
+    def _hash_filename(filename):
+        _, _, suffix = filename.rpartition('.')
+        return '%s.%s' %(uuid.uuid4().hex, suffix)
+
+    @cached_property
+    def symlink(self):
+        return short_url.encode_url(self.id)
+
+    @classmethod
+    def get_by_symlink(cls, symlink, code=404):
+        id = short_url.decode_url(cls.id)
+        return cls.query.filter_by(id=id).first() or abort(code)
+
+    @classmethod
+    def get_by_filehash(cls, filehash, code=404):
+        return cls.query.filter_by(filehash=filehash).first() or abort(code)
+
+    @classmethod
+    def get_by_md5(cls, filemd5, code=404):
+        return cls.query.filter_by(filemd5=filemd5).first() or abort(code)
+
+    @classmethod
+    def create_by_upload(cls, upload_file):
+        rst = cls(upload_file.filename, upload_file.mimetype, 0)
+        upload_file.save(rst.path)
+        with open(rst.path, 'rb') as f:
+            filemd5 = get_file_md5(f)
+            upload_file = cls.get_by_md5(filemd5)
+            if upload_file:
+                os.remove(rst.path)
+                return upload_file
+        filestat = os.stat(rst.path)
+        rst.size = filestat.st_size
+        rst.filemd5 = filemd5
 
 
 if __name__ == '__main__':
